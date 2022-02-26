137613b1bace2bcf0b7a1b051b4124603f41c020
Neil Purohit <58278838+NeilP06@users.noreply.github.com>
2022-02-26 14:14:27 -0600

    02-26-22

--- Finance.java
+++ Finance.java
@@ -1,1 +1,47 @@
-/** work on later */
+public class Finance {
+
+    private int balance;
+    private final int MAX_MONEY = Integer.MAX_VALUE;
+
+    public Finance(int theBalance) {
+        balance = theBalance;
+    }
+
+    /** Tells the system if the user is bankrupt: */
+    public boolean isBankrupt() {
+        if (balance > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /** Sets the balance for the user: */
+    public void setBalance(int newBalance) {
+        if (newBalance < MAX_MONEY) {
+            balance = newBalance;
+        }
+    }
+
+    /** Adds to the balance for the user: */
+    public void addBalance(int addedBalance) {
+        if (addedBalance < MAX_MONEY) {
+            balance += addedBalance;
+        }
+    }
+
+    /** Subtracts to the balance for the user: */
+    public void loseBalance(int lostBalance) {
+        balance -= lostBalance;
+    }
+
+    /** Returns the balance for the user: */
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public String toString() {
+        return balance + "";
+    }
+}
+
