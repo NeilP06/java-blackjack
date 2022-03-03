f46957d2272a8d4f79345fed33ddec15f9923e4f
Neil Purohit <58278838+NeilP06@users.noreply.github.com>
2022-03-02 20:23:24 -0600

    03-02-22

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nWelcome to Terminal Blackjack!\nYour options are: bet.\n- @author Neil Purohit 02-22-22");
+        try {
+            String command = scanner.nextLine();
+            if (command == "hit") {
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: don't type numbers. Rather only type strings.");
+        }
+        scanner.close();
+    }
+}
+
+/** NOTE: the code is not finished! */
